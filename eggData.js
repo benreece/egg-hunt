285d4f7c71a258a7f7f6f9d8723fd9414d327ae9
Ben Reece <benreece@gmail.com>
2023-04-05 21:16:08 -0600

    Initial commit

---
+++ eggData.js
@@ -0,0 +1,52 @@
+const eggData = [
+  {
+    clue: "Find me at the building where the Law School is located.",
+    location: { lat: 40.251878, lng: -111.649433 },
+    image: "egg1.png",
+    letter: "7",
+    found: false,
+  },
+  {
+    clue: "Find me at the building that is home to the Fulton College of Engineering and Technology.",
+    location: { lat: 40.249753, lng: -111.649158 },
+    image: "egg2.png",
+    letter: "9",
+    found: false,
+  },
+  {
+    clue: "Find me at the building named after a former LDS Church President and BYU President.",
+    location: { lat: 40.250813, lng: -111.651701 },
+    image: "egg3.png",
+    letter: "2",
+    found: false,
+  },
+  {
+    clue: "Find me at the building where the Life Sciences department is located.",
+    location: { lat: 40.251858, lng: -111.651048 },
+    image: "egg4.png",
+    letter: "4",
+    found: false,
+  },
+  {
+    clue: "Find me at the building where the College of Humanities is located.",
+    location: { lat: 40.250465, lng: -111.651357 },
+    image: "egg5.png",
+    letter: "+",
+    found: false,
+  },
+  {
+    clue: "Ben lives here",
+    location: { lat: 40.2565, lng: -111.7018 },
+    image: "egg6.png",
+    letter: "2",
+    found: false,
+  },
+  {
+    clue: "foo",
+    location: { lat: 40.2565, lng: -111.7018 },
+    image: "egg7.png",
+    letter: "J",
+    found: false,
+  },
+];
+
