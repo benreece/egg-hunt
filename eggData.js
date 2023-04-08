4cd5884a7e647e475273c8b44dc8a550131b3e4c
Ben Reece <benreece@gmail.com>
2023-04-07 19:56:36 -0600

    Use more accurate geocoordinates

--- eggData.js
+++ eggData.js
@@ -1,10 +1,9 @@
 const allEggData = {
-//OPTIONS: totes emoji
     'BYU': [
         {
             clue: "During the final week of His ministry the Savior cleansed the temple.<p>BYU's business school is in a building sometimes called 'the box the temple came in' due to its similar shape to the Salt Lake temple.</p><p>Latter-day temples often have 'Holiness to the Lord, The House of the Lord' written on the side; on most temples this inscription faces the same direction.</p><p>Go to this side of the temple box.</p>",
             response: "Read Matthew 21:12-13 and Discuss how the cleansing of the temple is a metaphor in your own lives.",
-            location: { lat: 40.251878, lng: -111.649433 },
+            location: { lat: 40.250442540755394, lng: -111.65178807272079 },
             image: "assets/eggs/egg1.png",
             letter: "7",
             found: false,
@@ -12,7 +11,7 @@ const allEggData = {
         {
             clue: "On the night of the passover dinner with His apostles the Savior instituted the sacrament (Luke 22:19-20).  This event is commemorated in an exhibit on campus.  Go to the main entrance of the building where the exhibit is on display.<p><img src='assets/byu/sacrament.jpeg' style='width: 100%;'>",
             response: "If the exhibit is open, take a few minutes to contemplate Of Souls and Sacraments.",
-            location: { lat: 40.249753, lng: -111.649158 },
+            location: { lat: 40.250879710983895, lng: -111.64771033378649 },
             image: "assets/eggs/egg2.png",
             letter: "9",
             found: false,
@@ -20,7 +19,7 @@ const allEggData = {
         {
             clue: "<p><a style='font-size: 5em;' onclick='playAudio(`assets/byu/song.mp3`);'>▶️ </a>",
             response: "One of the most common invitations from the Savior was 'Come, follow Me' (Luke 18:22).<p>A similar invitation peals across campus daily.<p>Consider the invitation from the Savior as you read William Clayton's inspired lyrics.",
-            location: { lat: 40.250813, lng: -111.651701 },
+            location: { lat: 40.252771620891046, lng: -111.64762704046983 },
             image: "assets/eggs/egg3.png",
             letter: "2",
             found: false,
@@ -28,7 +27,7 @@ const allEggData = {
         {
             clue: "The 4th location is aptly named:<p>Sides: 4<br>Sidewalks: 4<br>Triangles: 4<br>Pairs of benches: 4<br>Squares contained: 4<p>Find the center.",
             response: "Stand in the center of the quad and consider what we find with Christ at the center of our lives (see Isaiah 48:18).",
-            location: { lat: 40.251858, lng: -111.651048 },
+            location: { lat: 40.24973167558686, lng: -111.64927428437377 },
             image: "assets/eggs/egg4.png",
             letter: "4",
             found: false,
@@ -36,7 +35,7 @@ const allEggData = {
         {
             clue: "<p><img src='assets/byu/wordcloud.png'>",
             response: "",
-            location: { lat: 40.250465, lng: -111.651357 },
+            location: { lat: 40.250509796607545, lng: -111.64929112513362 },
             image: "assets/eggs/egg5.png",
             letter: "+",
             found: false,
@@ -44,7 +43,7 @@ const allEggData = {
         {
             clue: "<p style='font-size: 5em;'>🫘 🏛️   🫎  🗽</p>",
             response: "Christ created the Earth, the life upon it, and the light (John 1:3-4).  Reflect on Christ as the light of men.",
-            location: { lat: 40.2565, lng: -111.7018 },
+            location: { lat: 40.253641539013344, lng: -111.64715821239447 },
             image: "assets/eggs/egg6.png",
             letter: "2",
             found: false,
@@ -52,7 +51,7 @@ const allEggData = {
         {
             clue: "Eggs have come to be associated with Easter; some say they symbolize new life and represent the Resurrection (Luke 24:1-6).<p>An Easter egg on BYU campus can be seen from space.  Can you find the 'top' of the egg where it's wet?",
             response: "Rest a few moments next to the fountain and reflect how the Resurrection affects your discipleship of Christ.",
-            location: { lat: 40.2565, lng: -111.7018 },
+            location: { lat: 40.24843502997608, lng: -111.65119808808106 },
             image: "assets/eggs/egg7.png",
             letter: "J",
             found: false,
