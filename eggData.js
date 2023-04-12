1295ae855b268c2b54542137af898ea6b987b65e
Ben Reece <benreece@gmail.com>
2023-04-12 07:31:46 -0600

    Adjust final destination

--- eggData.js
+++ eggData.js
@@ -4,9 +4,6 @@ const allEggData = {
         'start_time': '2023-04-12 18:15 MDT',
         'start_clue': "<p><b>Welcome!</b><p>Find all the eggs to reveal the final clue!<p>Each egg you find will reveal a part of the final clue.  Feel free to use the internet, friends, strangers, and any other resources.<p>Start anywhere near or on BYU campus; visitor parking at the BYU Museum of Art is recommended.<p>The clue sequence has been randomized; the order that you choose to visit them will affect how quickly you complete the hunt.",
         'end_clue': "<p><b>Well done so far!</b><p>Once you solve the final clue, you may want to drive to the final destination.<p>When you arrive, say you're with the __ party.",
-        // front of the admin building: 7922+H7
-        // BYU Creamery on Ninth: 7924+2J
-        // Brick Oven: 68VV+RC
         'eggs': [
             {
                 clue: "During the final week of His ministry the Savior cleansed the temple.<p>BYU's business school is in a building sometimes called 'the box the temple came in' due to its similar shape to the Salt Lake temple.</p><p>Latter-day temples often have 'Holiness to the Lord, The House of the Lord' written on the side; on most temples this inscription faces the same direction.</p><p>Go to this side of the temple box.</p>",
@@ -53,7 +50,7 @@ const allEggData = {
                 response: "Christ created the Earth, the life upon it, and the light (John 1:3-4).  Reflect on Christ as the light of men.",
                 location: { lat: 40.253641539013344, lng: -111.64715821239447 },
                 image: "assets/eggs/egg6.png",
-                letter: "H",
+                letter: "P",
                 found: false,
             },
             {
@@ -62,133 +59,6 @@ const allEggData = {
                 location: { lat: 40.24843502997608, lng: -111.65119808808106 },
                 image: "assets/eggs/egg7.png",
                 letter: "7",
-                found: false,
-            },
-        ],
-    },
-    'BYU2': {
-        'title': '',
-        'start_time': '2023-04-08 08:00',
-        'start_clue': "<p><b>Welcome!</b><p>Find all the eggs to reveal the final clue!<p>Each egg you find will reveal a part of the final clue.  Feel free to use the internet, friends, strangers, and any other resources.<p>Start anywhere near or on BYU campus; visitor parking at the BYU Museum of Art is recommended.<p>The clue sequence has been randomized; the order that you choose to visit them will affect how quickly you complete the hunt.",
-        'end_clue': "<p><b>Well done so far!</b><p>Once you solve the final clue, you may want to drive to the final destination.<p>When you arrive, say you're with the __ party.",
-        // front of the admin building: 7922+H7
-        // BYU Creamery on Ninth: 7924+2J
-        // Brick Oven: 68VV+RC
-        'eggs': [
-            {
-                clue: "During the final week of His ministry the Savior cleansed the temple.<p>Find the main entrance to the building that bears the name of the most prolific temple dedicator.",
-                response: "Pres. Hinckley presided of the dedication or rededication of 90 temples.<p>Read Matthew 21:12-13 and discuss how the cleansing of the temple is a metaphor in your own lives.",
-                location: { lat: 40.25182499164598, lng: -111.65149182762926 },
-                image: "assets/eggs/egg1.png",
-                letter: "7",
-                found: false,
-            },
-            {
-                clue: "On the night of the passover dinner with His apostles the Savior instituted the sacrament (Luke 22:19-20).<p>The sacrament represents renewal; stand at the northwest corner of the building being destroyed on campus.",
-                response: "Contemplate how the sacrament affects your soul.",
-                location: { lat: 40.25033585784816, lng: -111.64885155663356 },
-                image: "assets/eggs/egg2.png",
-                letter: "9",
-                found: false,
-            },
-            {
-                clue: "<p>Botany Pond is a the south side of campus; did you know there's a pond on the north side, as well?  From here you have a great spot to listen to 'Come, Come, Ye Saints.'  Stand at the west side of the pond.",
-                response: "One of the most common invitations from the Savior was 'Come, follow Me' (Luke 18:22).<p>Consider the invitation from the Savior as you read William Clayton's inspired lyrics.",
-                location: { lat: 40.252424096714975, lng: -111.647169024426 },
-                image: "assets/eggs/egg3.png",
-                letter: "2",
-                found: false,
-            },
-            {
-                clue: "The largest glass pyramid on campus.  Stand north of its center.",
-                response: "Look at the center point of the pyramid and consider what we find with Christ at the center of our lives (see Isaiah 48:18).",
-                location: { lat: 40.249413726935096, lng: -111.64926716575589 },
-                image: "assets/eggs/egg4.png",
-                letter: "2",
-                found: false,
-            },
-            {
-                clue: "<p><img src='assets/byu/wordcloud2.png'><p>Southeast corner",
-                response: "<p>BYU's extracurricular programs provide opportunities for participants and fans to spread the gospel as they interact with others.<p>How does <i>your</i> example influence others?",
-                location: { lat: 40.25361551959949, lng: -111.64868089619317 },
-                image: "assets/eggs/egg5.png",
-                letter: "+",
-                found: false,
-            },
-            {
-                clue: "Seen from space it looks like someone started playing naughts &amp; crosses.<p style='font-size: 3em;'>o<br>x<br>x</p><p>Stand in the center of the bottom x.",
-                response: "Christ created the Earth, the life upon it, and the light (John 1:3-4).  Reflect on Christ as the light of men.",
-                location: { lat: 40.24972711354377, lng: -111.64927031540732 },
-                image: "assets/eggs/egg6.png",
-                letter: "H",
-                found: false,
-            },
-            {
-                clue: "On campus stands a statue of a peacemaker who welcomed strangers from across the ocean.<p>He established a peace that lasted his entire life.<p>His people taught the pilgrims how to survive in the wilderness of their new land.",
-                response: "Rest a few moments and reflect on the peace of the Savior.",
-                location: { lat: 40.248265359813026, lng: -111.64982627201948 },
-                image: "assets/eggs/egg7.png",
-                letter: "7",
-                found: false,
-            },
-        ],
-    },
-    'Parkway': {
-        'eggs': [
-            {
-                clue: "He's a stake president, but not ours.  He teaches piano.  That's right -- it's the Jensens!",
-                location: { lat: 40.247843, lng: -111.710551 },
-                image: "assets/eggs/egg1.png",
-                letter: "6",
-                found: false,
-            },
-            {
-                clue: "The Bishop lives here",
-                location: { lat: 40.247853, lng: -111.709065 },
-                image: "assets/eggs/egg2.png",
-                letter: "7",
-                found: false,
-            },
-            {
-                clue: "You might _Know_ where this past, aquatic bishop lives... 'cause he has a Finn... get it?!  Ha!",
-                location: { lat: 40.249505, lng: -111.712383 },
-                image: "assets/eggs/egg3.png",
-                letter: "X",
-                found: false,
-            },
-            {
-                clue: "Borther!",
-                location: { lat: 40.247700, lng: -111.713356 },
-                image: "assets/eggs/egg4.png",
-                letter: "P",
-                found: false,
-            },
-            {
-                clue: "Wooma!",
-                location: { lat: 40.246934, lng: -111.710235 },
-                image: "assets/eggs/egg5.png",
-                letter: "+",
-                found: false,
-            },
-            {
-                clue: "Deck destroyers",
-                location: { lat: 40.246936, lng: -111.709961 },
-                image: "assets/eggs/egg6.png",
-                letter: "F",
-                found: false,
-            },
-            {
-                clue: "Diminutive choir leaders live here",
-                location: { lat: 40.247850, lng: -111.710849 },
-                image: "assets/eggs/egg7.png",
-                letter: "M",
-                found: false,
-            },
-            {
-                clue: "Some really Good people live here",
-                location: { lat: 40.249492, lng: -111.712181 },
-                image: "assets/eggs/egg8.png",
-                letter: "J",
                 found: false,
             },
         ],
